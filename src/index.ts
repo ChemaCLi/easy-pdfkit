922aba432f6e797aa8cdc969962b7486e57617c0
Jose Maria CLi <chema9703@gmail.com>
2022-06-26 22:38:59 -0500

    Make a first approach for the generator input and internal mechanism

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import fs from "fs";
+import { DocGenerator } from "./modules";
+
+const docGenerator = new DocGenerator(
+  { margin: 10, font: { size: 20 } },
+  fs.createWriteStream('output.pdf')
+);
+
+docGenerator.generate([
+  {
+    mode: "content-blocks",
+    content: []
+  },
+  {
+    mode: "pages",
+    pages: [
+      {}
+    ]
+  }
+]);
